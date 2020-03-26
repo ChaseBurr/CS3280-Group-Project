b288626894345c75a77c4f7faa739d231a0afd7d
Chase <chase@chaseburr.com>
2020-03-26 10:37:15 -0600

    Added all the windows and classes needed for the project

--- CS3280 Group Project/Main/wndMain.xaml.cs
+++ CS3280 Group Project/Main/wndMain.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CS3280_Group_Project
+namespace CS3280_Group_Project.Items
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for wndMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class wndMain : Window
     {
-        public MainWindow()
+        public wndMain()
         {
             InitializeComponent();
         }
