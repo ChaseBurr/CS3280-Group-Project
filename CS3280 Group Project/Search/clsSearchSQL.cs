10443de95f14ec961aac1bf333cf0ceb4bcd1a73
Justin Edwards <jstnwrds55@gmail.com>
2020-03-28 15:54:39 -0600

    fixed dataAccess class connection string, added Search page UI and data population.

--- CS3280 Group Project/Search/clsSearchSQL.cs
+++ CS3280 Group Project/Search/clsSearchSQL.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace CS3280_Group_Project.Search
 {
-    class clsSearchSQL
-    {
+
+    class clsSearchSQL {
+        DataSet dataSet;
+        int numInvoices;
+        List<string> invoiceNumbers = new List<string>();
+        List<string> invoiceDates = new List<string>();
+        List<string> invoiceCosts = new List<string>();
+
+        public clsSearchSQL() {
+            // access to data
+            clsDataAccess dataAccess = new clsDataAccess();
+            dataSet = dataAccess.ExecuteSQLStatement("SELECT * FROM Invoices", ref numInvoices);
+
+            // store lists to populate displays
+            DataTable table = dataSet.Tables[0];
+            foreach (DataRow row in table.Rows) {
+                invoiceNumbers.Add(row.ItemArray[0].ToString());
+                invoiceDates.Add(row.ItemArray[1].ToString());
+                invoiceCosts.Add(row.ItemArray[2].ToString());
+            }
+
+        }
+
+        public List<string> GetInvoiceNumbers() {
+            return invoiceNumbers;
+        }
+
+        public List<string> GetInvoiceDates() {
+            return invoiceDates;
+        }
+
+        public List<string> GetInvoiceCosts() {
+            return invoiceCosts;
+        }
+
+        public DataSet GetDataSet() {
+            return dataSet;
+        }
+
+
     }
 }
--- CS3280 Group Project/Search/wndSearch.xaml.cs
+++ CS3280 Group Project/Search/wndSearch.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CS3280_Group_Project;
+using CS3280_Group_Project.Items;
+using CS3280_Group_Project.Search;
+using System.Data;
 
 namespace CS3280_Group_Project.Search
 {
@@ -19,9 +23,68 @@ namespace CS3280_Group_Project.Search
     /// </summary>
     public partial class wndSearch : Window
     {
+
+        clsSearchSQL searchSQL = new clsSearchSQL();
+        public string invoiceID { get; set; }
+
         public wndSearch()
         {
             InitializeComponent();
+            PopulateDropdowns();
+            PopulateDataGrid();
         }
+
+        /// <summary>
+        /// Clear selections in dropdown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearButton_Click(object sender, RoutedEventArgs e) {
+            InvoiceNumber.SelectedItem = null;
+            InvoiceDate.SelectedItem = null;
+            InvoiceCharge.SelectedItem = null;
+            
+        }
+
+        /// <summary>
+        /// Close window and send invoice back to main screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectButton_Click(object sender, RoutedEventArgs e) {
+            invoiceID = "5001";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void PopulateDropdowns() {
+            foreach (string invoiceNum in searchSQL.GetInvoiceNumbers()) {
+                InvoiceNumber.Items.Add(invoiceNum);
+            }
+            foreach (string invoiceDate in searchSQL.GetInvoiceDates()) {
+                InvoiceDate.Items.Add(invoiceDate.Substring(0, 9));
+            }
+            foreach (string invoiceCost in searchSQL.GetInvoiceCosts()) {
+                InvoiceCharge.Items.Add("$" + invoiceCost);
+            }
+        }
+
+        private void PopulateDataGrid() {
+            InvoiceDataGrid.AutoGenerateColumns = true;
+            InvoiceDataGrid.DataContext = searchSQL.GetDataSet().Tables[0].DefaultView;
+        }
+
+        /*
+        private void MenuItem_Click(object sender, RoutedEventArgs e) {
+            if ((sender as MenuItem).Header.ToString() == "Items" &&) {
+                wndItems window = new wndItems();
+                window.Show();
+            } else if ((sender as MenuItem).Header.ToString() == "Search") {
+                wndSearch window = new wndSearch();
+                window.Show();
+            }
+        }
+        */
     }
 }
