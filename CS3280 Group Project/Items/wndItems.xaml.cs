60e73c86096e5d3a73b5f004c9ddd2ede72beb68
Jeremy Dyson <jeremy-daniel@hotmail.com>
2020-03-27 21:11:06 -0600

    wndItems start.

--- CS3280 Group Project/Items/wndItems.xaml.cs
+++ CS3280 Group Project/Items/wndItems.xaml.cs
@@ -19,9 +19,91 @@ namespace CS3280_Group_Project.Items
     /// </summary>
     public partial class wndItems : Window
     {
+        /// <summary>
+        /// Flag indicating update to Items occurred
+        /// </summary>
+        public bool ItemListUpdated
+        {
+            get { return bItemListUpdated; }
+        }
+
+        /// <summary>
+        /// Flag for indicating update to Items
+        /// </summary>
+        private bool bItemListUpdated;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public wndItems()
         {
             InitializeComponent();
+            bItemListUpdated = false;
         }
+
+        /// <summary>
+        /// Add Item button click event for adding items
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddItem_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: calls business logic of clsItemsLogic.cs
+                // Logic needs to validate Item id does not exist already
+        }
+
+        /// <summary>
+        /// Delete Item button click event for record deletion
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: calls business logic of clsItemsLogic.cs. 
+                // Logic should not allow deletion if the item exists in an invoice
+                // Display error if error in deletion
+        }
+
+        /// <summary>
+        /// Edit Item button click event for editing Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEditItem_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: calls business logic of clsItemsLogic.cs
+                // Logic should not allow edit of the code.
+                // Only allows edit of the description and cost.
+        }
+
+        /// <summary>
+        /// List view selection event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // TODO: populate the Add/ Edit / Delete form window
+        }
+
+        /// <summary>
+        /// Window closing event 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // TODO: handle any notifications (if needed) back to main window
+        }
+
+        /// <summary>
+        /// Window loaded event used for setting default settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            bItemListUpdated = false;
+        }        
     }
 }
